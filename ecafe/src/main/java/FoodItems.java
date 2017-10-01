bc6ea1d8ead859f03571f434bad86fba09a5a8e4
RanaHashim <RanaHashim@users.noreply.github.com>
2017-10-01 10:35:18 +0500

    Add files via upload

---
+++ ecafe/src/main/java/FoodItems.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hashim Ali Khan
+ */
+public class FoodItems implements java.io.Serializable {
+    String name;
+    double price;
+    String type;
+    String description;
+   public void FoodItems(){
+   }
+}
