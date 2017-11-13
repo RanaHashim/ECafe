2d65d0ff196ecb2d5e93065629a2aa89c02bb999
RanaHashim <RanaHashim@users.noreply.github.com>
2017-11-13 13:14:39 +0500

    New files added

---
+++ src/main/java/com/hsquare/e_cafe1/FoodItems.java
@@ -0,0 +1,20 @@
+package com.hsquare.e_cafe1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hashim Ali Khan
+ */
+public class FoodItems implements java.io.Serializable {
+    String name;
+    double price;
+    String type;
+    int dish_id;
+   public void FoodItems(){
+   }
+}
